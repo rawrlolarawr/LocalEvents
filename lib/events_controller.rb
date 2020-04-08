318aa55415d9d77f35fa38e2de27fcc310d732ed
christopherc0825 <christopherc0825@gmail.com>
2020-04-08 17:15:40 -0400

    Removing responsibility of creating objects from the Scraper class and adding to the Controller class to follow MVC conventions

--- lib/events_controller.rb
+++ lib/events_controller.rb
@@ -3,20 +3,26 @@ class LocalEvents::EventsController
     include Checkable::InstanceMethods
     include Getable::InstanceMethods
 
-    #Input Validation Constants
+    # Source Constants
+
+    MYSTIC = {name: "Mystic Aquarium", url: "https://www.mysticaquarium.org/events/", css_calendar_tags: 'h2.tribe-events-list-event-title a', css_event_desc_tags: 'div.tribe-events-single-event-description p', css_event_date_time_tags: 'span.tribe-event-date-start'}
+    NCM = {name: "Niantic Children's Museum", url: "https://www.childrensmuseumsect.org/events/", css_calendar_tags: 'h3.tribe-events-month-event-title a', css_event_desc_tags: 'div.tribe-events-single-event-description p', css_event_date_time_tags: 'span.tribe-event-date-start'}
+
+    # Input Validation Constants
 
     MENU_RESPONSES = ["events", "source", "exit"]
     SOURCE_RESPONSES = ["1", "2", "exit"]
 
-    #Main Method
+    # Main Method
 
     def call
-        puts "\n\n\n\n\n\nLocal Events\n\n"
+        system("clear")
+        puts "Local Events\n\n"
         source_picker
         menu
     end
 
-    #CLI Methods
+    # CLI Methods
 
     # Chooses a source to retrieve events from based on input
     def source_picker
@@ -26,9 +32,9 @@ class LocalEvents::EventsController
         if valid?(SOURCE_RESPONSES, input)
             case input
             when "1"
-                scrape_and_or_set_calendar("Mystic Aquarium")
+                scrape_and_or_set_calendar(MYSTIC)
             when "2"
-                scrape_and_or_set_calendar("Niantic Children's Museum")
+                scrape_and_or_set_calendar(NCM)
             when "exit"
                 quit
             end
@@ -58,20 +64,27 @@ class LocalEvents::EventsController
         end
     end
 
-    #Calendar Methods
+    # Calendar Methods
 
     # Checks to see if the calendar instance based on source is created.
-    # If it is the method sets the Calendar to that instance if not it scrapes the source.
-    def scrape_and_or_set_calendar(name)
-        if LocalEvents::Calendar.find_by_name(name)
-            @current_calendar = LocalEvents::Calendar.find_by_name(name)
+    # If it is the method sets the Calendar to that instance if not it scrapes the source and creates the Calendar and its Events.
+    def scrape_and_or_set_calendar(source)
+        if LocalEvents::Calendar.find_by_name(source[:name])
+            @current_calendar = LocalEvents::Calendar.find_by_name(source[:name])
         else
-            LocalEvents::Scraper.scrape(name)
-            scrape_and_or_set_calendar(name)
+            @current_calendar = LocalEvents::Calendar.new(source)
+            LocalEvents::Scraper.scrape_calendar(@current_calendar)
+            create_events
         end
     end
 
-    #Event Methods
+    # Event Methods
+
+    # Creates Events based on @current_calendar
+    def create_events
+        events = LocalEvents::Scraper.scrape_events(@current_calendar)
+        events.each {|attributes| LocalEvents::Event.new(attributes)}
+    end
 
     # Lists the events based on the Calendar that was set.
     def list_events
@@ -95,7 +108,7 @@ class LocalEvents::EventsController
 
     # Populates and prints the event that is passed
     def populate_and_display_event(event)
-        LocalEvents::Scraper.scrape_event_info(event)
+        LocalEvents::Scraper.scrape_event_details(event)
         printer(["#{event.name}", "#{event.date} at #{event.time}\n\n", "#{event.description}", "Website: #{event.url}\n\n"])
     end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,24 +1,15 @@
 class LocalEvents::Scraper
-    #Source Constants
-    
-    MYSTIC = {name: "Mystic Aquarium", url: "https://www.mysticaquarium.org/events/", css_calendar_tags: 'h2.tribe-events-list-event-title a', css_event_desc_tags: 'div.tribe-events-single-event-description p', css_event_date_time_tags: 'span.tribe-event-date-start'}
-    NCM = {name: "Niantic Children's Museum", url: "https://www.childrensmuseumsect.org/events/", css_calendar_tags: 'h3.tribe-events-month-event-title a', css_event_desc_tags: 'div.tribe-events-single-event-description p', css_event_date_time_tags: 'span.tribe-event-date-start'}
+    # Main Method
 
-    #Main Method
-
-    # Creates a Calendar instance based on the source passed
-    # Scrapes the webpage from the url in the Calendar created
-    def self.scrape(source)
-        if source == "Mystic Aquarium"
-            calendar = LocalEvents::Calendar.new(MYSTIC)
+    # Scrapes the webpage from the url in the Calendar passed in
+    def self.scrape_calendar(calendar)
+        if calendar.name == "Mystic Aquarium"
             calendar.doc = Nokogiri::HTML(open(calendar.url))
             scrape_calendar_content(calendar)
-        elsif source == "Niantic Children's Museum"
-            calendar = LocalEvents::Calendar.new(NCM)
+        elsif calendar.name == "Niantic Children's Museum"
             calendar.doc = Nokogiri::HTML(open(calendar.url))
             scrape_calendar_content(calendar)
         end
-        create_events(calendar)
     end
 
     # Calendar Methods
@@ -29,18 +20,17 @@ class LocalEvents::Scraper
         calendar.event_names = calendar.doc.css(calendar.css_calendar_tags).map {|name| name.text.strip}
     end
 
-    #Event Methods
+    # Event Methods
 
-    # Creates Event instances based on the names and links from the Calendar
-    def self.create_events(current_calendar)
-        current_calendar.event_links.each_with_index do |link, i|
+    # Scrapes Events from the current calendar and returns the attributes in an array of hashes
+    def self.scrape_events(current_calendar)
+        current_calendar.event_links.collect.with_index do |link, i|
             attributes = {name: current_calendar.event_names[i], url: link, calendar: current_calendar}
-            LocalEvents::Event.new(attributes)
         end
     end
 
     # Scrapes description, date, and time from the Event link attribute
-    def self.scrape_event_info(event)
+    def self.scrape_event_details(event)
         event_page = Nokogiri::HTML(open(event.url))
         event.description = event_page.css(event.calendar.css_event_desc_tags).first.text
         event.date = event_page.css(event.calendar.css_event_date_time_tags).first.text.split(" @ ").first
